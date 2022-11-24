8bc78ddb46113540ac7887328a712e0754139267
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-24 11:43:50 +0300

    commit tree 7a9e8b1eb8bb8157fc230b8ada0e8277f98f869b

--- app/controllers/plants_conroller.rb
+++ app/controllers/plants_conroller.rb
@@ -1,28 +1,25 @@
 class PlantsController < ApplicationController
+    def index
+        render json: Plant.all, status: :ok
+      end
 
-    wrap_parameters formart: []
-    def index 
-        plant = Plant.all
-        render json: plant, status: :ok
-    end
-
-    def show 
+    def show
         plant = Plant.find_by(id: params[:id])
-        if plant
-            render json: plant, status: :ok 
+        if plant != nil
+          render json: plant,status: :ok
         else
-            render json: { error: "Plant not found" }, status: :not_found
+          render json: {error: "plant not found"}, status: :not_found
         end
     end
 
-    def create 
+    def create
         plant = Plant.create(plant_params)
         render json: plant, status: :created
     end
 
     private
 
-    def plant_params 
+    def plant_params
         params.permit(:name, :image, :price)
     end
 end
